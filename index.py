b836b892a6cbb14fc057ce56c9382f258c81a666
Derek Lam <dc.lam@mail.utoronto.ca>
2017-08-06 19:11:25 -0400

    Replace GOES-16 cloudmask with GOES-East + CLAVR-X algorithm
    Found a different data source:
    http://cimss.ssec.wisc.edu/clavrx/google_earth/images/clavrx_goes13_YYYY_zzz_HHmm/cmask.png
    
    Updates every 15 minutes, and it's already in EPSG:4326. Some artefacts of
    the reprojection near the poles, but this is vastly more convenient than
    the HDF, which maps lonlats to raster pixels; I don't even know how I would
    project the sky intercept lonlats onto the image.
    
    Note that it takes the KML at
    http://cimss.ssec.wisc.edu/clavrx/google_earth/goes_east_kml/goes_east.html ,
    not the image. The KML is needed for georeferencing.

--- index.py
+++ index.py
@@ -6,13 +6,15 @@ import sys
 import math
 
 from scipy.spatial import KDTree
+from xml.dom import minidom
+import os
+import shutil
+import requests
 import numpy as np
 import Pysolar.solar as pysolar
 # import requests
 import cv2
 import gdal
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
 
 def contour_depth(idx, hierarchy):
 	if hierarchy[0][idx][2] == -1:
@@ -35,88 +37,89 @@ def poly_centroid(P):
 
 def travel(origin, heading, distance): # distance in radians, heading is from true north
 	lat = math.asin(math.sin(origin[1]) * math.cos(distance) + math.cos(origin[1]) * math.sin(distance) * math.cos(heading))
-	return (\
-		origin[0] - math.asin(math.sin(heading) * math.sin(distance) / math.cos(lat)) % (math.pi * 2),\
-		lat\
-	)
+	return np.asarray((
+		origin[0] + math.asin(math.sin(heading) * math.sin(distance) / math.cos(lat)),
+		lat
+	))
 
-class GOES16Cloudmask:
-	def __init__(self):
-		# physical properties
-		self.RHO = 6370
-		self.ALTITUDE = 35790
-		self.ORIGIN = [ -math.pi / 2, 0 ]
-		self.ALMOST_PHI = [ float(self.RHO) / (self.ALTITUDE + self.RHO) * math.sqrt(math.pow(self.ALTITUDE + self.RHO, 2) - math.pow(self.RHO, 2)), ((math.pow(self.ALTITUDE + self.RHO, 2) - math.pow(self.RHO, 2)) / (self.ALTITUDE + self.RHO)) ]
-		
-		# image properties
-		self.IMG_MIDPOINT = np.asarray([313, 356])
-		self.GLOBE_DIAMETER = 655
-		
-	def _sat_angle(self, alpha):
-		return math.atan2(alpha * self.ALMOST_PHI[0], self.ALMOST_PHI[1])
-	
-	def _cartesian(self, spherical): # expect tuple of lon-lat
-		return np.asarray([
-			math.cos(spherical[0]) * math.cos(spherical[1]),
-			math.sin(spherical[0]) * math.cos(spherical[1]),
-			-math.sin(spherical[1])
-		])
-	
-	def deproject(self, img_coords):
-		origin_distance = math.sqrt(np.linalg.norm(img_coords - self.IMG_MIDPOINT))
-		sat_angle = self._sat_angle(origin_distance / self.GLOBE_DIAMETER * 2)
-		great_angle = -sat_angle + math.asin((self.ALTITUDE + self.RHO) / self.RHO * math.sin(sat_angle))
-		chord = math.sin(great_angle) * self.RHO
-		azimuth_angle = math.atan2(img_coords[1] - self.IMG_MIDPOINT[1], img_coords[0] - self.IMG_MIDPOINT[0])
-		lon = math.asin(math.cos(azimuth_angle) * chord / math.sqrt(math.pow(self.RHO, 2) - math.pow(chord * math.sin(azimuth_angle), 2))) + self.ORIGIN[1]
-		lat = -math.asin(math.sin(azimuth_angle) * chord / self.RHO) + self.ORIGIN[1]
-		return np.asarray([lon, lat])
-		
-	def project(self, geo_coords):
-		rho_vec = self._cartesian(geo_coords) * self.RHO
-		n_hat = np.asarray([ 0, -1, 0 ]) # pointing out of 90\deg W at equator
-		sat_to_point_vec = rho_vec - np.asarray([ 0, -self.RHO - self.ALTITUDE, 0 ])
-		sat_to_point_hat = sat_to_point_vec / np.linalg.norm(sat_to_point_vec)
-		return (((math.pow(self.RHO, 2) / (self.ALTITUDE + self.RHO)) - (np.dot(rho_vec, n_hat)) / np.dot(sat_to_point_hat, n_hat)) * sat_to_point_hat + rho_vec)[0::2] / self.RHO * self.GLOBE_DIAMETER / 2 + self.IMG_MIDPOINT
-		
 if __name__ == '__main__':
 	HUNDRED_FEET_TO_KM = 0.3048 * 100 * 0.001
 
-	# sys.argv expect: [__FILE__, cloudmask, radar tiff, metar json, elevation]
+	# sys.argv expect: [__FILE__, cloudmask KML, radar tiff, metar json, elevation]
 	f_cloudmask, f_radar, f_metar, f_out = sys.argv[1:] # f_elevation
 	f_radar_wld = '%s.wld' % '.'.join(f_radar.split('.')[:-1])
 	# need to open radar with image lib to convert to greyscale; openCV?
 	radar = cv2.imread(f_radar, cv2.IMREAD_GRAYSCALE)
+	
 	with open(f_radar_wld, 'r') as radar_wld, open(f_metar, 'r') as metar, open(f_out, 'a') as outfile:
 		# radar params
-		[dx, _, _, dy, nwx, nwy] = [float(v.strip()) for v in list(radar_wld)]
-		nw = np.asarray([nwx, nwy])
-		dv = np.asarray([dx, dy])
+		[radar_dx, _, _, radar_dy, radar_nwx, radar_nwy] = [float(v.strip()) for v in list(radar_wld)]
+		radar_nw = np.asarray([radar_nwx, radar_nwy])
+		radar_dv = np.asarray([radar_dx, radar_dy])
 		
-		# GOES-16 cloudmask helpers
-		proj = GOES16Cloudmask()
-		globe_mask = cv2.imread('data/globe_mask.png', cv2.IMREAD_GRAYSCALE)
-		markings = cv2.imread('data/markings.png', cv2.IMREAD_GRAYSCALE)
-		border_mask = cv2.imread('data/borders.png', cv2.IMREAD_GRAYSCALE)
+		cloudmask_kml = minidom.parse(f_cloudmask)
+		overlays = cloudmask_kml.getElementsByTagName('GroundOverlay')
+		for overlay in overlays:
+			if overlay.getElementsByTagName('name')[0].childNodes[0].nodeValue.strip() == 'Cloud Mask':
+				url = overlay.getElementsByTagName('Icon')[0].getElementsByTagName('href')[0].childNodes[0].nodeValue
+				
+				# wld params
+				latlonbox = overlay.getElementsByTagName('LatLonBox')[0]
+				bounds = [
+					float(latlonbox.getElementsByTagName('north')[0].childNodes[0].nodeValue),
+					float(latlonbox.getElementsByTagName('south')[0].childNodes[0].nodeValue),
+					float(latlonbox.getElementsByTagName('east')[0].childNodes[0].nodeValue),
+					float(latlonbox.getElementsByTagName('west')[0].childNodes[0].nodeValue)
+				]
+				
+				cloudmask_response = requests.get(url, stream=True)
+				d, f = url.split('/')[-2:]
+				if not os.path.exists('/tmp/cmasks/%s' % d):
+					os.makedirs('/tmp/cmasks/%s' % d)
+					
+				with open('/tmp/cmasks/%s/%s' % (d, f), 'wb') as tmpfile:
+					cloudmask_response.raw.decode_content = True
+					shutil.copyfileobj(cloudmask_response.raw, tmpfile)
+				
+				cloudmask = cv2.imread('/tmp/cmasks/%s/%s' % (d, f), cv2.IMREAD_GRAYSCALE)
+				cloudmask_nw = np.asarray([ bounds[3], bounds[0] ])
+				cloudmask_dv = np.divide(
+					np.asarray([bounds[2] - bounds[3], bounds[1] - bounds[0]]),
+					np.asarray(cloudmask.shape)
+				)
+				break
 		
-		cloudmask = cv2.imread(f_cloudmask)
-		cardinal_directions = itertools.permutations([-1, 0, 1], 2)
-		for i, row in enumerate(border_mask):
-			for j, pix in enumerate(row):
-				if pix > 0:
-					for direction in cardinal_directions:
-						if globe_mask[i + direction[0], j + direction[1]] > 0 and not border_mask[i + direction[0], j + direction[1]]:
-							cloudmask[i, j] = cloudmask[i + direction[0], j + direction[1]]
+		# cloudmask_hdf = gdal.Open(f_cloudmask)
+		# cloudmask = gdal.Open(cloudmask_hdf.GetSubDatasets()[30][0]).ReadAsArray()
+		# lon = gdal.Open(cloudmask_hdf.GetSubDatasets()[6][0]).ReadAsArray()
+		# lat = gdal.Open(cloudmask_hdf.GetSubDatasets()[5][0]).ReadAsArray()
+		# cloudmask_lookup = []
+		# cloudmask_coords = []
 		
-		cloudmask = np.logical_and(
-			globe_mask > 0,
-			np.logical_or.reduce((
-				cv2.inRange(cloudmask, (0, 0, 255), (0, 0, 255)), # ice cloud
-				cv2.inRange(cloudmask, (0, 255, 255), (0, 255, 255)), # ice cloud weak
-				cv2.inRange(cloudmask, (255, 255, 0), (255, 255, 0)), # supercooled water cloud
-				cv2.inRange(cloudmask, (255, 0, 0), (255, 0, 0)) # water cloud
-			))
-		)
+		# for i in range(cloudmask.shape[0]):
+		# 	for j in range(cloudmask.shape[1]):
+		# 		cloudmask_lookup.append(cloudmask[i, j])
+		# 		cloudmask_coords.append([ lon[i, j], lat[i, j] ])
+		
+		# cloudmask_kdtree = KDTree(cloudmask_coords)
+		
+		# cardinal_directions = itertools.permutations([-1, 0, 1], 2)
+		# for i, row in enumerate(border_mask):
+		# 	for j, pix in enumerate(row):
+		# 		if pix > 0:
+		# 			for direction in cardinal_directions:
+		# 				if globe_mask[i + direction[0], j + direction[1]] > 0 and not border_mask[i + direction[0], j + direction[1]]:
+		# 					cloudmask[i, j] = cloudmask[i + direction[0], j + direction[1]]
+		
+		# cloudmask = np.logical_and(
+		# 	globe_mask > 0,
+		# 	np.logical_or.reduce((
+		# 		cv2.inRange(cloudmask, (0, 0, 255), (0, 0, 255)), # ice cloud
+		# 		cv2.inRange(cloudmask, (0, 255, 255), (0, 255, 255)), # ice cloud weak
+		# 		cv2.inRange(cloudmask, (255, 255, 0), (255, 255, 0)), # supercooled water cloud
+		# 		cv2.inRange(cloudmask, (255, 0, 0), (255, 0, 0)) # water cloud
+		# 	))
+		# )
 		
 		metar_geojson = json.load(metar)
 		metar_lookup = [feature['properties'] for feature in metar_geojson['features']]
@@ -145,7 +148,7 @@ if __name__ == '__main__':
 			print('\r%d / %d contours completed, %d skipped, %d sky pixels hit' % (contour_idx - 1, len(radar_contours), contours_skipped, len(sky_points)), end='', file=sys.stderr)
 			if len(px_contour) > 2:
 				# where all the magic happens
-				contour = np.multiply(np.squeeze(px_contour).astype(np.float32), dv) + nw
+				contour = np.multiply(np.squeeze(px_contour).astype(np.float32), radar_dv) + radar_nw
 				ceil = None
 				# visited = set() # eh, not worth it.
 				for num_stations_exponent in itertools.count(1):
@@ -177,16 +180,17 @@ if __name__ == '__main__':
 					# use small-angle approximation so that the pythag output is still radians
 					T = datetime.datetime(2017, 07, 27, 22, 15)
 					sky_intercept = travel(
-						P * math.pi / 180,
-						math.pi + (pysolar.GetAzimuth(P[1], P[0], T) * math.pi / 180), # pysolar uses latlon; we use lonlat
-						ceil * HUNDRED_FEET_TO_KM / math.tan(pysolar.GetAltitude(P[1], P[0], T) * math.pi / 180) / proj.RHO # TEMP
-					)
-					cloudmask_coord = proj.project(sky_intercept)
-					cloudmask_px = cloudmask[int(round(cloudmask_coord[1])), int(round(cloudmask_coord[0]))]
+P * math.pi / 180,
+math.pi - (pysolar.GetAzimuth(P[1], P[0], T) * math.pi / 180), # pysolar uses latlon; we use lonlat
+ceil * HUNDRED_FEET_TO_KM / math.tan(pysolar.GetAltitude(P[1], P[0], T) * math.pi / 180) / 6370 # TEMP: earth radius will find a better home later
+)
+					cloudmask_coords = np.divide(sky_intercept * 180 / math.pi - cloudmask_nw, cloudmask_dv).astype(np.uint16)
+					cloudmask_px = cloudmask[cloudmask_coords[1], cloudmask_coords[0]]
 					
-					if not cloudmask_px:
+					if cloudmask_px == 0 or \
+						cloudmask_px == 42:
 					   outfile.write('[%s, %s],\n' % tuple(P))
-					   sky_points.add((int(round(cloudmask_coord[1])), int(round(cloudmask_coord[0]))))
+					   sky_points.add((int(round(cloudmask_coords[1])), int(round(cloudmask_coords[0]))))
 					
 					# clear_flag = True
 					# for idx, cloud_poly in enumerate(cloud_polies):
@@ -198,8 +202,3 @@ if __name__ == '__main__':
 					# if clear_flag:
 					# 	# rainbow_points.append(P)
 					# 	outfile.write('[%s, %s],\n' % tuple(P))
-		
-		gdal.Close(cloudmask_raw)
-		gdal.Close(lat_raw)
-		gdal.Close(lon_raw)
-	gdal.Close(cloudmask_hdf)
